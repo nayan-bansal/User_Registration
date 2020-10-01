0490df4e224fab0a9aee2199e1e4862ce6986591
nayan-bansal <nayan.7991@gmail.com>
2020-10-01 14:05:50 +0530

    Added: Welcome Message

---
+++ Registration.java
@@ -0,0 +1,15 @@
+package Registration;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Registration {
+	public static void main(String[] args) {
+		
+		//Printing Welcome Message
+		System.out.println("Welcome to User Registration Program");
+		
+		
+	}
+	
+}
