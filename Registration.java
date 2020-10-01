bfc51028f4cf97ac0381e832333f137d63a09c41
nayan-bansal <nayan.7991@gmail.com>
2020-10-01 14:11:29 +0530

    Added: Last Name Pattern

--- Registration.java
+++ Registration.java
@@ -28,7 +28,22 @@ public class Registration {
 		else
 		System.out.println("First Name does not follows the pattern");
 		
-	
+		//Adding Last Name
+		
+		System.out.println("Enter Last Name: ");
+		String last_name  = scan.next();
+		
+		//Matching Pattern
+		
+		Pattern last_name_pattern =  Pattern.compile(pattern);
+		Matcher m1 = last_name_pattern.matcher(last_name);
+		
+		if(m1.matches())
+		System.out.println("Last Name Follows Pattern :"+last_name);
+		else
+		System.out.println("Last Name does not follows the pattern");
+		
+		
 	}
 	
 }
