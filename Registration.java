3188c598b5150fef09779b2297d0135d49981427
nayan-bansal <nayan.7991@gmail.com>
2020-10-01 14:09:00 +0530

    Added: First Name Pattern

--- Registration.java
+++ Registration.java
@@ -9,7 +9,26 @@ public class Registration {
 		//Printing Welcome Message
 		System.out.println("Welcome to User Registration Program");
 		
+		//Adding First Name
 		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter First Name: ");
+		String first_name  = scan.next();
+		
+		String pattern = "^[A-Z]{1}[a-z]{2,}$";
+		
+		//Matching Pattern
+		
+		Pattern first_name_pattern =  Pattern.compile(pattern);
+		Matcher m = first_name_pattern.matcher(first_name);
+		
+		if(m.matches())
+		System.out.println("First Name Follows Pattern :"+first_name);
+		else
+		System.out.println("First Name does not follows the pattern");
+		
+	
 	}
 	
 }
