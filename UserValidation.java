dd9c168d698f9b43e5c7b1b5b073bc19007a1f42
nayan-bansal <nayan.7991@gmail.com>
2020-10-09 14:23:36 +0530

    Added: Validation method

--- UserValidation.java
+++ UserValidation.java
@@ -5,40 +5,25 @@ import org.junit.Test;
 
 public class Validation {
 
-	@Test
-	public void positiveTest() {
-		boolean expectedFirstName=true;
-		boolean actualFirstName= Registration.first_name("Nayan");
-		assertEquals(expectedFirstName,actualFirstName);
-		boolean expectedLastName=true;
-		boolean actualLastName=Registration.last_name("Bansal");
-		assertEquals(expectedLastName,actualLastName);
-		boolean expectedEMail=true;
-		boolean actualEMail=Registration.email_id("abc@yahoo.com");
-		assertEquals(expectedEMail,actualEMail);
-		boolean expectedMobileNumber=true;
-		boolean actualMobileNumber=Registration.phone_number("91 9416029025");
-		assertEquals(expectedMobileNumber,actualMobileNumber);
-		boolean expectedPassword=true;
-		boolean actualPassword=Registration.password("Asdf1234%");
-		assertEquals(expectedPassword,actualPassword);
-
+	private String Input;
+	private boolean Output;
+	private Registration eMailChecker;
+	@Before
+	public void initialize() {
+		eMailChecker=new Registration();
 	}
-	public void negativeTest() {
-		boolean expectedFirstName=false;
-		boolean actualFirstName=Registration.first_name("nayan");
-		assertEquals(expectedFirstName,actualFirstName);
-		boolean expectedLastName=false;
-		boolean actualLastName=Registration.last_name("bansal");
-		assertEquals(expectedLastName,actualLastName);
-		boolean expectedEMail=false;
-		boolean actualEMail=Registration.email_id("abc@com");
-		assertEquals(expectedEMail,actualEMail);
-		boolean expectedMobileNumber=false;
-		boolean actualMobileNumber=Registration.phone_number("911 992331990");
-		assertEquals(expectedMobileNumber,actualMobileNumber);
-		boolean expectedPassword=false;
-		boolean actualPassword=Registration.password("dfff1234");
-		assertEquals(expectedPassword,actualPassword);
+	public MultipleEntryTest(String Input,boolean Output) {
+		this.Input=Input;
+		this.Output=Output;
+	}
+	@Parameterized.Parameters
+	public static Collection eMails() {
+		return Arrays.asList(new Object [][] {{"abc@yahoo.com",true},{"abc@com",false},{"abc@1.com",true}});
+	}
+	@Test
+	public void testMultipleEntryTest() {
+		//fail("Not yet implemented");
+		System.out.println("Input: "+Input);
+		assertEquals(Output,Registration.eMailCheck(Input));
 	}
 }
