854315aaaf6698b9c90764e0107125124603f25a
nayan-bansal <69717938+nayan-bansal@users.noreply.github.com>
2020-11-25 12:03:19 +0530

    Update EntryTest.java

--- EntryTest.java
+++ EntryTest.java
@@ -4,16 +4,16 @@ public class UserEntryTest {
 	@Test
 	public void happyTest() {
 		boolean expectedFirstName=true;
-		boolean actualFirstName=Registration.firstNameCheck("Tarush");
+		boolean actualFirstName=Registration.firstNameCheck("Nayan");
 		assertEquals(expectedFirstName,actualFirstName);
 		boolean expectedLastName=true;
-		boolean actualLastName=Registration.lastNameCheck("Goel");
+		boolean actualLastName=Registration.lastNameCheck("Bansal");
 		assertEquals(expectedLastName,actualLastName);
 		boolean expectedEMail=true;
 		boolean actualEMail=Registration.eMailCheck("abc@yahoo.com");
 		assertEquals(expectedEMail,actualEMail);
 		boolean expectedMobileNumber=true;
-		boolean actualMobileNumber=Registration.mobileNumberCheck("91 9919819901");
+		boolean actualMobileNumber=Registration.mobileNumberCheck("91 9416029025");
 		assertEquals(expectedMobileNumber,actualMobileNumber);
 		boolean expectedPassword=true;
 		boolean actualPassword=Registration.passwordCheck("Asdf1234%");
@@ -21,16 +21,16 @@ public class UserEntryTest {
 	}
 	public void sadTest() {
 		boolean expectedFirstName=false;
-		boolean actualFirstName=Registration.firstNameCheck("tarush");
+		boolean actualFirstName=Registration.firstNameCheck("nayan");
 		assertEquals(expectedFirstName,actualFirstName);
 		boolean expectedLastName=false;
-		boolean actualLastName=Registration.lastNameCheck("goel");
+		boolean actualLastName=Registration.lastNameCheck("bansal");
 		assertEquals(expectedLastName,actualLastName);
 		boolean expectedEMail=false;
 		boolean actualEMail=Registration.eMailCheck("abc@com");
 		assertEquals(expectedEMail,actualEMail);
 		boolean expectedMobileNumber=false;
-		boolean actualMobileNumber=Registration.mobileNumberCheck("911 991981990");
+		boolean actualMobileNumber=Registration.mobileNumberCheck("911 9416029025");
 		assertEquals(expectedMobileNumber,actualMobileNumber);
 		boolean expectedPassword=false;
 		boolean actualPassword=Registration.passwordCheck("asdf1234");
